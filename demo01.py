6cb05261cea49b2dff207e43a5c6042d507027ca
ropz <270314985@qq.com>
2020-10-26 16:22:38 +0800

    第一次向github提交代码

---
+++ demo01.py
@@ -0,0 +1,15 @@
+# encoding:utf-8
+# print(type("哈哈"))
+# print(type(2333))
+# print(type(2.333))
+# print(type(True))
+# print(type(()))
+# print(type([]))
+# print(type({}))
+# a = int("2333")
+# print(type(a))
+# a = "哈哈"
+# print(len(a))
+a = input("请输入：")
+b = input("请输入：")
+print("input获取的内容:",len(a+b))
